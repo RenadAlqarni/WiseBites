663db58bfe4ebdadb330a4c8b42859154391349c
RenadAlqarni <renadsalqarni@gmail.com>
2024-12-20 19:35:44 +0300

    Initial Commit

---
+++ WiseBites/WiseBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiseBitesApp.swift
+//  WiseBites
+//
+//  Created by Renad Alqarni on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WiseBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WiseBitesTests/WiseBitesTests.swift
@@ -0,0 +1,17 @@
+//
+//  WiseBitesTests.swift
+//  WiseBitesTests
+//
+//  Created by Renad Alqarni on 20/12/2024.
+//
+
+import Testing
+@testable import WiseBites
+
+struct WiseBitesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
